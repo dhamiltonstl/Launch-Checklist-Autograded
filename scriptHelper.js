64bba6cd17b933beb78b05c37bef6729fa25da85
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-04-30 11:33:02 -0500

    validateInput written & some formSubmission written

--- scriptHelper.js
+++ scriptHelper.js
@@ -15,30 +15,67 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                  </ol>
                  <img src="">
     */
- }
- 
- function validateInput(testInput) {
-    
- }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
- 
- module.exports.addDestinationInfo = addDestinationInfo;
- module.exports.validateInput = validateInput;
- module.exports.formSubmission = formSubmission;
- module.exports.pickPlanet = pickPlanet; 
- module.exports.myFetch = myFetch;
+}
+
+function validateInput(testInput) {
+    if (testInput === "") return "Empty";
+    else if (isNaN(testInput)) return "Not a Number"
+    else return "Is a Number"
+}
+
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    const pilotStatus = document.getElementById("pilotStatus");
+    const copilotStatus = document.getElementById("copilotStatus");
+    const fuelStatus = document.getElementById("fuelStatus");
+    const cargoStatus = document.getElementById("cargoStatus");
+    const h2 = document.querySelector("h2");
+
+    let ready = true;
+
+    if (validateInput(pilot) === "Not a Number") pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    else alert("Invalid Pilot Name");
+
+    if (validateInput(copilot) === "Not a Number") copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    else alert("Invalid Copilot Name");
+
+    if (validateInput(fuelLevel) === "Is a Number") {
+        if (fuelLevel < "10000") {
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            ready = false;
+        };
+    } else alert("Invalid Fuel Level");
+
+    if (validateInput(cargoLevel) === "Is a Number") {
+        if (cargoLevel > "10000") {
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+            ready = false;
+        };
+    } else alert("Invalid Cargo Level")
+    console.log(list.style.visiblity)
+    if (ready === true) {
+        h2.innerHTML = "Shuttle is Ready for Launch";
+        h2.style.color = "green";
+    } else {
+        h2.innerHTML = "Shuttle Not Ready for Launch";
+        h2.style.color = "red";
+        list.style.visiblity = "visible";
+    }
+}
+
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch().then(function (response) {
+    });
+
+    return planetsReturned;
+}
+
+function pickPlanet(planets) {
+}
+
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
