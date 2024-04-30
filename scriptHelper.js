be4c92c4082715258ca6f2d51e1657a01daa4a80
Derek Hamilton <125328155+dhamiltonstl@users.noreply.github.com>
2024-04-30 14:12:34 -0500

    completed form submission

--- scriptHelper.js
+++ scriptHelper.js
@@ -24,6 +24,7 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    
     const pilotStatus = document.getElementById("pilotStatus");
     const copilotStatus = document.getElementById("copilotStatus");
     const fuelStatus = document.getElementById("fuelStatus");
@@ -32,34 +33,42 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
     let ready = true;
 
+    // console.log("FUEL STATUS: ", fuelStatus.innerHTML)
+
     if (validateInput(pilot) === "Not a Number") pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
     else alert("Invalid Pilot Name");
 
     if (validateInput(copilot) === "Not a Number") copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
     else alert("Invalid Copilot Name");
 
-    if (validateInput(fuelLevel) === "Is a Number") {
-        if (fuelLevel < "10000") {
-            fuelStatus.innerHTML = "Fuel level too low for launch";
-            ready = false;
-        };
-    } else alert("Invalid Fuel Level");
-
+    // console.log("CARGO VALIDATION: ", validateInput(cargoLevel))
     if (validateInput(cargoLevel) === "Is a Number") {
-        if (cargoLevel > "10000") {
+        if (cargoLevel > 10000) {
+            // console.log("CARGO TOO HEAVY", cargoLevel)
             cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             ready = false;
-        };
+        } else cargoStatus.innerHTML = "Cargo mass low enough for launch"
     } else alert("Invalid Cargo Level")
-    console.log(list.style.visiblity)
+
+    // console.log("FUEL VALIDATION: ", validateInput(fuelLevel))
+    if (validateInput(fuelLevel) === "Is a Number") {
+        if (fuelLevel < 10000) {
+            // console.log("FUEL TOO LOW", fuelLevel)
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            ready = false;
+        } else fuelStatus.innerHTML = "Fuel level high enough for launch"
+    } else alert("Invalid Fuel Level");
+
+    // console.log("READY: ", ready)
     if (ready === true) {
         h2.innerHTML = "Shuttle is Ready for Launch";
         h2.style.color = "green";
     } else {
         h2.innerHTML = "Shuttle Not Ready for Launch";
         h2.style.color = "red";
-        list.style.visiblity = "visible";
+        list.style.visibility = "visible";
     }
+    // console.log("END", list.style.visiblity)
 }
 
 async function myFetch() {
